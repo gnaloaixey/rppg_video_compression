fc21077589847faff32ae6ad72571b3ee29492d3
gnaloaixey <gnaloaixey@gmail.com>
2023-09-26 10:24:41 +0800

    UBFC_Phys dataset loader

--- dataset_loader/UBFC_Phys.py
+++ dataset_loader/UBFC_Phys.py
@@ -1,5 +1,7 @@
 import pandas as pd
-
+import numpy as np
+import os
+import re
 class DatasetLoader:
     root = ''
     loader_name = 'UBFC_Phys'
@@ -7,7 +9,24 @@ class DatasetLoader:
         self.root = root
         pass
     def load_data(self):
-        return pd.DataFrame()
-    def ppg_reader(self,path):
-        df = pd.read_csv(path)
-        return df
+        list_of_video_path = []
+        list_of_ppg_data = []
+        contents = os.listdir(self.root)
+        for content in contents:
+            content_path = os.path.join(self.root, content)  # 获取内容的完整路径
+            for root, dirs, files in os.walk(content_path):
+                rgb_video_path = ''
+                ppg_data_path = ''
+                for file in files:
+                    if re.search(r'^vid_(.+)_T3\.avi$', file) :  # 找到视频文件
+                        rgb_video_path = os.path.join(root, file)
+                    elif re.search(r'^bvp_(.+)_T3\.csv$', file):
+                        ppg_data_path = os.path.join(root, file)  # PPG文件  
+                if os.path.exists(rgb_video_path) and os.path.exists(ppg_data_path):
+                    list_of_video_path.append(rgb_video_path)
+                    list_of_ppg_data.append( self.__ppg_reader(ppg_data_path))
+        return np.array(list_of_video_path),np.array(list_of_ppg_data)
+    def __ppg_reader(self,path):
+        ppg_df = pd.read_csv(path,header=None)
+        ppg_data = ppg_df.iloc[:,0].to_numpy(dtype=np.float64)
+        return ppg_data
---
+++ singleton_pattern/load_data_generator.py
@@ -0,0 +1,12 @@
+from importlib import import_module as __import_module
+from singleton_pattern import load_config as __load_config
+__config = __load_config.get_config()
+__generator = None
+def get_tensor_data_generator():
+    global __generator
+    if __generator == None:
+        method_name = __config['method']
+        model_file = __import_module(f'data_generator.{method_name}')
+        DataGenerator = getattr(model_file,'DataGenerator')
+        __generator = DataGenerator()
+    return __generator
