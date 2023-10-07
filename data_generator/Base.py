9a54d129841395d443e4df6c06d7327c881271da
gnaloaixey <gnaloaixey@gmail.com>
2023-10-07 11:48:41 +0800

    Base config

--- data_generator/Base.py
+++ data_generator/Base.py
@@ -4,10 +4,18 @@ from singleton_pattern import load_config
 from util.ppg_interpolat import generate_interpolated_ppg
 from util.face_detection import read_video_and_generate_factor,get_face_shape
 class DataGenerator:
-    def generate_tensor_data(self,data):
+    batch_size:int
+    time_slice_interval:float
+    time_step:float
+    def __init__(self) -> None:
         config = load_config.get_config()
+        data_format = config['data_format']
+        self.batch_size = data_format['batch_size']
+        self.time_slice_interval = data_format['time_slice_interval']
+        self.time_step = data_format['time_step']
+        pass
+    def generate_tensor_data(self,data):
         video_paths,ppgs = data
-        batch_size = config['batch_size']
         for i,video_path in enumerate(video_paths):
             y =  generate_interpolated_ppg(ppgs[i],video_path)
             # 压缩
